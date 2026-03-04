8d3244a3a0d1442d982295959562b47858f7d72e
r1xez <slavik534226@gmail.com>
2026-03-04 14:47:38 +0200

    /

--- Library_update/Program.cs
+++ Library_update/Program.cs
@@ -59,9 +59,9 @@ namespace Library_update
                 });
 
             builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection")
-    ));
+                options.UseSqlServer(
+            builder.Configuration.GetConnectionString("DefaultConnection")
+                ));
 
             builder.Services.AddHttpClient<IGutendexService, GutendexService>(client =>
             {
